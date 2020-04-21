6fd3560528f71d8a40c3add163e7d49166b67859
phani-srikar <phanisrikar93ume@gmail.com>
2020-04-21 08:39:26 -0700

    add resources for AWSApiGatewayTests

---
+++ src/integ_test_resources/ios-sdk/cdkIntegrationTestsIos/cdk_integration_tests_ios/cdk_integration_tests_ios_stack.py
@@ -0,0 +1,25 @@
+from aws_cdk import(
+    core,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw
+)
+
+
+class CdkIntegrationTestsIosStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        echo_lambda = _lambda.Function(
+            self, 'EchoHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='echo.handler',
+        )
+
+        apiEndpoint = apigw.LambdaRestApi(
+                        self, 'Endpoint',
+                        handler=echo_lambda,
+                      )
+        print(apiEndpoint)
+
