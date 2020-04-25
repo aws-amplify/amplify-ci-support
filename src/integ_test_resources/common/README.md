385260870d977dcee16e733698513a3151dbe62d
Jameson Williams <jhwill@amazon.com>
2020-04-24 20:59:49 -0500

    Generate Android test configuration data from script
    `device_config_builder.py` can be used to create the contents of a
    `testconfiguration.json`, as expected by the Android and iOS SDK's
    integration test suites.
    
    To use it:
    ```
    ./device_config_builder.py ios
    ```
    Or:
    ```
    ./device_config_builder.py android
    ```
    
    The script will produce a JSON structure that looks like this:
    ```
    {
      "Credentials": {
        "accessKey": "blah",
        "secretKey": "blah",
        "sessionToken": "blah"
      },
      "Packages": {
        "pinpoint": {
          "key1": "val2"
        },
        "textract": {
          "key1": "val2"
        }
      }
    }
    ```
    
    The credentials are taken from the following environment variables:
      1. AWS_ACCESS_KEY_ID
      2. AWS_SECRET_ACCESS_KEY
      3. AWS_SESSION_TOKEN
    
    The package data is taken from the ouputs of a call to SSM
    `get-parameters-by-path`. The credentials from the environment varaibles
    above are used to make the call. Only parameters that begin
    `/mobile-sdk/<platform>` are considered. Parameters are expected to have
    the form: `/mobile-sdk/<platform>/<suitelabel>/<keypath>`.
    
    `<suitelabel>` is like `pinpoint` or `textract`. `<platform>` is like
    `ios` or `android`.
    
    `<keypath>` may be a simple string, or itself may be a compound like
    `foo/bar/baz`. If `<keypath>` contains `/`, then the path is interpreted
    as a sequence of nested JSON objects. For example, if the key path is
    `foo/bar`, and the value to be stored is `value`, the JSON
    representation would look like:
    ```
    "suitelabel: {
        "foo": {
            "bar": "value"
        }
    }
    ```

--- src/integ_test_resources/common/README.md
+++ src/integ_test_resources/common/README.md
@@ -1,1 +1,67 @@
-Stub directory to be filled out with resource management scripts & templates
+# Common resources for both iOS and Android
+
+### Device Config File Builder
+
+`device_config_builder.py` can be used to create the contents of a
+`testconfiguration.json`, as expected by the Android and iOS SDK's
+integration test suites.
+
+To use it:
+```
+./device_config_builder.py ios
+```
+Or:
+```
+./device_config_builder.py android
+```
+
+The script will produce a JSON structure that looks like this:
+```
+{
+  "Credentials": {
+    "accessKey": "blah",
+    "secretKey": "blah",
+    "sessionToken": "blah"
+  },
+  "Packages": {
+    "pinpoint": {
+      "key1": "val2"
+    },
+    "textract": {
+      "key1": "val2"
+    }
+  }
+}
+```
+
+The credentials are taken from the following environment variables:
+  1. `AWS_ACCESS_KEY_ID`
+  2. `AWS_SECRET_ACCESS_KEY`
+  3. `AWS_SESSION_TOKEN`
+
+The package data is taken from the ouputs of a call to SSM
+`get-parameters-by-path`. The credentials from the environment varaibles
+above are used to make the call. Only parameters that begin
+`/mobile-sdk/<platform>` are considered. Parameters are expected to have
+the form: `/mobile-sdk/<platform>/<suitelabel>/<keypath>`.
+
+`<suitelabel>` is like `pinpoint` or `textract`.
+
+`<platform>` is like `ios` or `android`.
+
+`<keypath>` may be a simple string, or itself may be a compound like
+`foo/bar/baz`. If `<keypath>` contains `/`, then the path is interpreted as a
+sequence of nested JSON objects. For example, if the key path is `foo/bar`, and
+the value to be stored is `value`, the JSON representation would look like:
+```
+"suitelabel: {
+    "foo": {
+        "bar": "value"
+    }
+}
+```
+
+------------------
+
+[amplify.aws](https://amplify.aws)
+
---
+++ src/integ_test_resources/common/test/device_config_builder_test.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import unittest
+
+import sys
+import pathlib
+sys.path.append(str(pathlib.Path(__file__).parent.absolute()) + '/..')
+from device_config_builder import DeviceConfigBuilder
+
+class TestDeviceConfigBuilder(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestDeviceConfigBuilder, self).__init__(*args, **kwargs)
+        self.underTest = DeviceConfigBuilder('android')
+
+    def test_build_package_data(self):
+        prefix = '/mobile-sdk/android'
+        parameters = self.load_parameters()
+
+        package_data = self.underTest.build_package_data(prefix, parameters)
+
+        self.assertEqual(
+            {
+                'apigateway': {
+                    'api_key': 'someapikeyhere',
+                    'endpoint_us_east_1': 'https://redactedapi.execute-api.us-east-1.amazonaws.com/prod/',
+                    'endpoint_us_east_2': 'https://redactedapi.execute-api.us-east-2.amazonaws.com/prod',
+                },
+                'core': {
+                    'identity_pool_id': 'us-east-1:00000000-3c37-43d7-8fe6-3e0a65985b02'
+                }
+            },
+            package_data
+        )
+
+    def test_build_package_data_with_nesting(self):
+        params = [
+            {
+                "Name": "/mobile-sdk/android/suite/foo/bar/baz",
+                "Value": "foo-bar-baz-val"
+            },
+            {
+                "Name": "/mobile-sdk/android/suite/potato/bar/baz",
+                "Value": "potato-bar-baz-val"
+            },
+        ]
+        prefix = '/mobile-sdk/android'
+
+        package_data = self.underTest.build_package_data(prefix, params)
+
+        self.assertEqual(
+            {
+                "suite": {
+                    "foo": {
+                        "bar": {
+                            "baz": "foo-bar-baz-val"
+                        }
+                    },
+                    "potato": {
+                        "bar": {
+                            "baz": "potato-bar-baz-val"
+                        }
+                    }
+                }
+            },
+            package_data
+        )
+
+
+    def test_aws_config_from_environment(self):
+        environment_variables = {
+            'AWS_ACCESS_KEY_ID': 'accessKey',
+            'AWS_SECRET_ACCESS_KEY': 'secretKey',
+            'AWS_SESSION_TOKEN': 'sessionToken',
+            'AWS_DEFAULT_REGION': 'defaultRegion'
+        }
+        for key, value in environment_variables.items():
+            os.environ[key] = value
+
+        aws_config = self.underTest.aws_config_from_environment()
+
+        self.assertEquals(
+            DeviceConfigBuilder.AWSConfig('accessKey', 'secretKey', 'sessionToken', 'defaultRegion'),
+            aws_config
+        )
+
+    def load_parameters(self):
+        """
+        This method "stubs" out the result of calling SSM's
+        get-parameters-by-prefix
+        """
+        current_dir_abs_path = str(pathlib.Path(__file__).parent.absolute())
+        parameter_json_abs_path = current_dir_abs_path + '/all-parameters.json'
+        data = ''
+        with open (parameter_json_abs_path, "r") as parameters_file:
+            data = ' '.join(parameters_file.readlines())
+
+        return json.loads(data)['Parameters']
+
+if __name__ == '__main__':
+    unittest.main()
+
