9923f1e1372deec39c8c52dd48727daccfc23b3c
John Pignata <john@pignata.com>
2020-04-25 10:37:04 -0400

    Add stack for API Gateway tests
    Creates resources required to successfully run
    aws-android-sdk-apigateway-test, including:
    
    - A RestApi with a UsagePlan and an ApiKey (set to a known value)
    - Resources that methods that act as a proxy to httpbin.org for loopback
    
    Three parameters are output:
    
    - The endpoint of the RestApi
    - An endpoint of an arbitrary, non-existent RestApi
    - A static ApiKey

--- src/integ_test_resources/android/sdk/integration/cdk/app.py
+++ src/integ_test_resources/android/sdk/integration/cdk/app.py
@@ -4,9 +4,12 @@ from aws_cdk import core
 
 from cdk_integration_tests_android.pinpoint_stack import PinpointStack
 from cdk_integration_tests_android.textract_stack import TextractStack
+from cdk_integration_tests_android.apigateway_stack import ApiGatewayStack
 
 app = core.App()
-PinpointStack(app, "cdk-integration-tests-android-pinpoint")
-TextractStack(app, "cdk-integration-tests-android-textract")
+
+ApiGatewayStack(app, 'cdk-integration-tests-android-apigateway')
+PinpointStack(app, 'cdk-integration-tests-android-pinpoint')
+TextractStack(app, 'cdk-integration-tests-android-textract')
 
 app.synth()
---
+++ src/integ_test_resources/android/sdk/integration/cdk/cdk_integration_tests_android/apigateway_stack.py
@@ -0,0 +1,70 @@
+import random
+
+from aws_cdk import aws_apigateway as apigateway
+from aws_cdk import core
+
+
+class ApiGatewayStack(core.Stack):
+    HTTPBIN_URL_TEMPLATE = 'http://httpbin.org/{method}'
+    ENDPOINT = 'https://{id}.execute-api.us-east-2.amazonaws.com/prod'
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Create API
+        api = apigateway.RestApi(self, 'testApi')
+
+        # Generate a random API key, add it to the underlying CloudFormation
+        # resource of the Construct so that we can use it in our tests.
+        api_key_value = self.random_hex(20)
+        api_key = api.add_api_key('testKey')
+        cfn_api_key = api_key.node.default_child
+        cfn_api_key.add_property_override('Value', api_key_value)
+
+        # Create Usage Plan and add it to the API
+        plan = api.add_usage_plan('usagePlan', api_key=api_key)
+        plan.add_api_stage(stage=api.deployment_stage)
+
+        # Create resources in API
+        echo = api.root.add_resource('echo')
+        echo_post = echo.add_resource('post')
+        auth = api.root.add_resource('auth')
+        apikey = api.root.add_resource('apikey')
+
+        # Wire up methods
+        get_url = self.HTTPBIN_URL_TEMPLATE.format(method='get')
+        post_url = self.HTTPBIN_URL_TEMPLATE.format(method='post')
+
+        get_integration = apigateway.HttpIntegration(get_url)
+        post_integration = apigateway.HttpIntegration(post_url,
+                                                      http_method='POST')
+        options_integration = apigateway.HttpIntegration(get_url,
+                                                         http_method='OPTIONS')
+        head_integration = apigateway.HttpIntegration(get_url,
+                                                      http_method='HEAD')
+
+        echo.add_method('GET', get_integration)
+        echo.add_method('OPTIONS', options_integration)
+        echo.add_method('HEAD', head_integration)
+        echo_post.add_method('POST', post_integration)
+        auth.add_method('GET', get_integration,
+                        authorization_type=apigateway.AuthorizationType.IAM)
+        apikey_method = apikey.add_method('GET', get_integration,
+                                          api_key_required=True)
+
+        # Generate all methods for /echo
+        for method in ('put', 'post', 'delete', 'patch'):
+            url = self.HTTPBIN_URL_TEMPLATE.format(method=method)
+            integration = apigateway.HttpIntegration(url, http_method=method)
+            echo.add_method(method.upper(), integration)
+
+        # Output endpoint of the API, the pretend endpoint, and API key
+        pretend_endpoint = self.ENDPOINT.format(id=self.random_hex(10))
+
+        core.CfnOutput(self, 'endpoint_us_east_1', value=api.url)
+        core.CfnOutput(self, 'endpoint_us_east_2', value=pretend_endpoint)
+        core.CfnOutput(self, 'api_key', value=api_key_value)
+
+    def random_hex(self, length):
+        rand = '%x' % random.randrange(10**80)
+        return rand[:length]
