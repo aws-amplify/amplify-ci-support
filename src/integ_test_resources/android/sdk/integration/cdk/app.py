6ceff7e5030c5ec04da72bc7aa35c7ea0a08bb96
John Pignata <john@pignata.com>
2020-04-25 16:24:42 -0400

    Add stack for core resources (#14)
    Creates resources required to successfully run aws-android-sdk-core
    which is limited to a single Cognito identity pool. Note there is some
    coupling between this suite and the Kinesis suite, and I've left those
    tests to be resolved later.

--- src/integ_test_resources/android/sdk/integration/cdk/app.py
+++ src/integ_test_resources/android/sdk/integration/cdk/app.py
@@ -2,13 +2,16 @@
 
 from aws_cdk import core
 
+from cdk_integration_tests_android.apigateway_stack import ApiGatewayStack
+from cdk_integration_tests_android.core_stack import CoreStack
 from cdk_integration_tests_android.pinpoint_stack import PinpointStack
 from cdk_integration_tests_android.textract_stack import TextractStack
-from cdk_integration_tests_android.apigateway_stack import ApiGatewayStack
+
 
 app = core.App()
 
 ApiGatewayStack(app, 'cdk-integration-tests-android-apigateway')
+CoreStack(app, 'cdk-integration-tests-android-core')
 PinpointStack(app, 'cdk-integration-tests-android-pinpoint')
 TextractStack(app, 'cdk-integration-tests-android-textract')
 
