623e740ca98eff197d19ce818442bd83a2286e76
phani-srikar <phanisrikar93ume@gmail.com>
2020-04-29 15:52:04 -0700

    add region check for services to make sure they are available

--- src/integ_test_resources/ios/sdk/integration/cdk/cdk_integration_tests_ios/mobileclient_stack.py
+++ src/integ_test_resources/ios/sdk/integration/cdk/cdk_integration_tests_ios/mobileclient_stack.py
@@ -6,18 +6,22 @@ from aws_cdk import(
 )
 from parameter_store import save_string_parameter
 from auth_utils import construct_identity_pool
+from common_stack import CommonStack
+from cdk_stack_extension import CDKStackExtension
 
-class MobileClientStack(core.Stack):
+class MobileClientStack(CDKStackExtension):
 
     def __init__(self,
                  scope: core.Construct,
                  id: str,
-                 circleci_execution_role: aws_iam.Role,
+                 common_stack: CommonStack,
                  **kwargs) -> None:
 
         super().__init__(scope,
                          id,
                          **kwargs)
+
+        self._supported_in_region = self.is_service_supported_in_region("cognito-identity")
 
         user_pool = aws_cognito.UserPool(self,
                                          "userpool",
@@ -43,9 +47,10 @@ class MobileClientStack(core.Stack):
         save_string_parameter(self, "userpool_id", user_pool.user_pool_id)
         save_string_parameter(self, "pool_id_dev_auth", identity_pool.ref)
 
-        circleci_execution_role.add_to_policy(aws_iam.PolicyStatement(effect=aws_iam.Effect.ALLOW,
-                                                                      actions=[
-                                                                          "cognito-identity:*"],
-                                                                      resources=["*"]
-                                                                      ))
+        stack_policy = aws_iam.PolicyStatement(effect=aws_iam.Effect.ALLOW,
+                                               actions=[
+                                                   "cognito-identity:*",
+                                               ],
+                                               resources=["*"])
 
+        common_stack.add_to_common_role_policies(self, policy_to_add=stack_policy)
