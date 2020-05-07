44826b5601edcfb9af9aa7eca4b141977b21a16a
Tim Schmelter <schmelte@amazon.com>
2020-05-07 16:18:15 -0700

    More IoT policies

--- src/integ_test_resources/ios/sdk/integration/cdk/cdk_integration_tests_ios/iot_stack.py
+++ src/integ_test_resources/ios/sdk/integration/cdk/cdk_integration_tests_ios/iot_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import aws_iam, aws_lambda, core, custom_resources
+from aws_cdk import aws_iam, aws_iot, aws_lambda, core, custom_resources
 
 from common.common_stack import CommonStack
 from common.platforms import Platform
@@ -10,7 +10,15 @@ class IotStack(RegionAwareStack):
         super().__init__(scope, id, **kwargs)
 
         self._supported_in_region = self.is_service_supported_in_region("iot")
+        self._parameters_to_save = {}
 
+        self.setup_custom_resource()
+        self.setup_test_policies(common_stack)
+        self.setup_iot_policy()
+
+        self.save_parameters_in_parameter_store(platform=Platform.IOS)
+
+    def setup_custom_resource(self):
         describe_endpoint_policy = aws_iam.PolicyStatement(
             effect=aws_iam.Effect.ALLOW, actions=["iot:DescribeEndpoint"], resources=["*"],
         )
@@ -40,7 +48,38 @@ class IotStack(RegionAwareStack):
             service_token=provider.service_token,
         )
 
-        self._parameters_to_save = {
-            "iot_endpoint_address": iot_endpoint.get_att("EndpointAddress").to_string()
-        }
-        self.save_parameters_in_parameter_store(platform=Platform.IOS)
+        endpoint_address_attribute = iot_endpoint.get_att("EndpointAddress")
+
+        self._parameters_to_save["iot_endpoint_address"] = endpoint_address_attribute.to_string()
+
+    def setup_test_policies(self, common_stack):
+        specific_resources_policy = aws_iam.PolicyStatement(
+            effect=aws_iam.Effect.ALLOW,
+            actions=["iot:AttachPrincipalPolicy"],
+            resources=[f"arn:aws:iot:{self.region}:{self.account}:cert/*"],
+        )
+        common_stack.add_to_common_role_policies(self, policy_to_add=specific_resources_policy)
+
+        all_resources_policy = aws_iam.PolicyStatement(
+            effect=aws_iam.Effect.ALLOW, actions=["iot:CreateCertificateFromCsr"], resources=["*"],
+        )
+        common_stack.add_to_common_role_policies(self, policy_to_add=all_resources_policy)
+
+    def setup_iot_policy(self):
+        iot_policy = aws_iot.CfnPolicy(
+            self,
+            "iot-integ-test-policy",
+            policy_document={
+                "Version": "2012-10-17",
+                "Statement": [
+                    {"Effect": "Allow", "Action": "iot:Connect", "Resource": "*"},
+                    {
+                        "Effect": "Allow",
+                        "Action": ["iot:Publish", "iot:Subscribe", "iot:Receive"],
+                        "Resource": "*",
+                    },
+                ],
+            },
+            policy_name="iot-integ-test-policy",
+        )
+        self._parameters_to_save["iot_policy_name"] = iot_policy.policy_name
