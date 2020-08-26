e8f108fb5592bb6d7d84dc211b4d6da46c74e270
Kyle Yan <kyleyan@cs.washington.edu>
2020-08-26 12:29:06 -0700

    fix error in error message

--- src/fastlane/release_actions/lib/fastlane/plugin/release_actions/helper/key_value.rb
+++ src/fastlane/release_actions/lib/fastlane/plugin/release_actions/helper/key_value.rb
@@ -3,6 +3,7 @@ class KeyValue
     # Will match just the value that's contained inside of either single or double quotes
     # E.g., if key = AMPLIFY_VERSION and the file contains: $AMPLIFY_VERSION = "1.3.3"
     # it will match 1.3.3
+    @key = key
     @regex_key = /(#{key}\s*[=:]\s*["']?\K)(\^?[\d\w.-]?)*/
   end
 
@@ -10,7 +11,7 @@ class KeyValue
     file_contents = File.read(file)
 
     unless match(file_contents)
-      raise StandardError, "#{key} not present or doesn't have an explicit value in #{file}"
+      raise StandardError, "#{@key} not present or doesn't have an explicit value in #{file}"
     end
 
     file_contents = replace(file_contents: file_contents, value: value)
