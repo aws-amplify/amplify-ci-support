6adbfd1154129c0744edd83cee8950626d1df522
Rafael Juliano <rjjulian@amazon.com>
2020-09-10 12:16:42 -0400

    Cleanup of the app file

--- src/build_infrastructure/android/app.py
+++ src/build_infrastructure/android/app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
+import os
 from aws_cdk import core
 from cdk_build_pipeline.build_pipeline_stack import BuildPipelineStack
 
 app = core.App()
+TARGET_REGION = app.node.try_get_context("region")
+TARGET_ACCOUNT = app.node.try_get_context("account")
+TARGET_ENV=core.Environment( account=TARGET_ACCOUNT, region=TARGET_REGION)
 
-region = app.node.try_get_context("region")
-account = app.node.try_get_context("account")
-if region is None or account is None:
-    raise ValueError(
-        "Provide region and account in 'context' parameter, as in: cdk deploy app -c region=us-west-2 -c account=123456"  # noqa: E501
-    )
+print(f"AWS Account={TARGET_ACCOUNT} Region={TARGET_REGION}")
 
 pipeline_stack = BuildPipelineStack(app, 
                                     "AndroidBuildPipeline",
                                     {'pipeline_name':'amplify-android-pipeline', 'codebuild_project_name':'amplify-android-gradle-build'},
-                                    env={ 'account': account, 'region': region})
+                                    env=TARGET_ENV)
+
 app.synth()
