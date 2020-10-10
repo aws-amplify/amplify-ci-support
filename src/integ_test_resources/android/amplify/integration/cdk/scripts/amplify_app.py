340710d144a4176b0f7c8550da7f4d7150114d74
Rafael Juliano <rjjulian@amazon.com>
2020-10-09 20:36:55 -0400

    Refactored modules and deploy scripts

---
+++ src/integ_test_resources/android/amplify/integration/cdk/scripts/amplify_app.py
@@ -0,0 +1,132 @@
+import json
+import os
+from common import (
+    LOGGER,
+    AMPLIFY_AWSSDK_CLIENT,
+    BASE_PATH,
+    run_command,
+    OperationType
+)
+from auth import *
+
+AMPLIFY_COMMAND = "amplify"
+AMPLIFY_ACTION_PULL = "pull"
+AMPLIFY_ACTION_INIT = "init"
+AMPLIFY_ENVIRONMENT = "main" # Static for now
+
+AMPLIFY_PROVIDER_CONFIG = {
+    'awscloudformation': {
+        'configLevel': 'general',
+        'region': 'us-east-1'
+    }
+}
+
+AMPLIFY_FRONTEND_CONFIG = {
+    'frontend': 'android',
+    'config' : {
+        'ResDir': f"app/src/main/res"
+    }
+}
+
+AMPLIFY_CODEGEN_CONFIG_ANDROID = {
+    'generateCode': True,
+    'codeLanguage': 'java',
+    'generateDocs': True
+}
+
+class AmplifyApp:
+    app_name = None
+    app_id = None
+    project_dir = None
+    amplify_meta_content = None
+
+    def __init__(self, *, app_name:str):
+        self.app_name = app_name
+        self.project_dir = f"{BASE_PATH}/_amplify_{self.app_name}"
+        os.system(f"mkdir -p {self.project_dir}")
+        self.app_id = self._get_existing_app_id()
+        if self.app_id is None and self._initialize_new_app() != 0:
+            raise Exception("Unable to initialize Amplify project.")
+        else:
+            self._pull_existing_app()
+        with open(f"{self.project_dir}/amplify/backend/amplify-meta.json") as amplify_meta_file:
+            self.amplify_meta_content = json.load(amplify_meta_file)
+        self._load_metadata()
+
+    def is_category_configured(self, category_name:str):
+        if self.amplify_meta_content is None:
+            return False
+        return category_name in self.amplify_meta_content.keys()
+
+    def config_auth(self, auth_config, op_type:OperationType):
+        cmd = [AMPLIFY_COMMAND,
+                        op_type.value,
+                        "auth",
+                        "--headless"]
+        result = run_command(cmd, work_dir=self.project_dir, input=json.dumps(auth_config))
+        return result.returncode
+
+    def config_api(self, api_config, op_type:OperationType):
+        cmd = [AMPLIFY_COMMAND,
+                        op_type.value,
+                        "api",
+                        "--headless"]
+        result = run_command(cmd, work_dir=self.project_dir, input=json.dumps(api_config))
+        return result.returncode
+        
+    def get_category_config(self, category_name: str):
+        if self.amplify_meta_content is None:
+            return None
+        return category_name in self.amplify_meta_content.keys()
+
+    def push(self):
+        push_cmd = [AMPLIFY_COMMAND,
+                    "push",
+                    "--codegen", json.dumps(AMPLIFY_CODEGEN_CONFIG_ANDROID),
+                    "--yes"]
+        result = run_command(push_cmd, self.project_dir)
+        self._load_metadata()
+        return result.returncode
+
+    def _get_existing_app_id(self):
+        try:
+            response = AMPLIFY_AWSSDK_CLIENT.list_apps()
+            existing_app_id = next(app['appId'] for app in response['apps'] if app['name'] == self.app_name)
+            return existing_app_id
+        except StopIteration:
+            LOGGER.error(f"Unable to find existing Amplify app for {self.app_name}")
+            return None
+
+    def _initialize_new_app(self):
+        init_cmd = [AMPLIFY_COMMAND, 
+                    AMPLIFY_ACTION_INIT, 
+                    "--amplify", json.dumps(self._get_amplify_config()), 
+                    "--providers", json.dumps(AMPLIFY_PROVIDER_CONFIG), 
+                    "--frontend", json.dumps(AMPLIFY_FRONTEND_CONFIG)]
+        result = run_command(init_cmd, self.project_dir)
+        self.app_id = self._get_existing_app_id()
+        return result.returncode
+
+
+    def _pull_existing_app(self):
+        amplify_params = self._get_amplify_config().copy()
+        amplify_params.update({'appId': self.app_id})
+        pull_cmd = [AMPLIFY_COMMAND, 
+                    AMPLIFY_ACTION_PULL, 
+                    "--amplify", json.dumps(amplify_params), 
+                    "--providers", json.dumps(AMPLIFY_PROVIDER_CONFIG), 
+                    "--frontend", json.dumps(AMPLIFY_FRONTEND_CONFIG),
+                    "--yes"]
+        result = run_command(pull_cmd, self.project_dir)
+        return result.returncode
+
+    def _get_amplify_config(self):
+        return {
+            'projectName': self.app_name,
+            'envName': AMPLIFY_ENVIRONMENT,
+            'defaultEditor': 'code'
+        }
+
+    def _load_metadata(self):
+        with open(f"{self.project_dir}/amplify/backend/amplify-meta.json") as amplify_meta_file:
+            self.amplify_meta_content = json.load(amplify_meta_file)
--- src/integ_test_resources/android/amplify/integration/cdk/stacks/amplify_deployer_stack.py
+++ src/integ_test_resources/android/amplify/integration/cdk/stacks/amplify_deployer_stack.py
@@ -12,8 +12,7 @@ class AmplifyDeployer(core.Stack):
     DEFAULT_BRANCH = "refs/heads/main"
     def __init__(self, scope: core.App, id: str, props, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
-        build_file_path = 'src/integ_test_resources/android/amplify/integration/cdk/scripts/buildspec.yml'
+        cb_buildspec_file_path = props['cb_buildspec_file_path']
         github_repo = props['github_repo']
         github_owner  = self.DEFAULT_GITHUB_OWNER if 'github_owner' not in props else props['github_owner']
         branch  = self.DEFAULT_BRANCH if 'branch' not in props else props['branch']
@@ -29,33 +28,67 @@ class AmplifyDeployer(core.Stack):
                                                                             branch_or_ref=branch,
                                                                             webhook=False), # Will need to setup creds to make this true
                                         environment=build_environment,
-                                        build_spec=aws_codebuild.BuildSpec.from_source_filename(filename=build_file_path))
+                                        build_spec=aws_codebuild.BuildSpec.from_source_filename(filename=cb_buildspec_file_path))
         individual_actions = [
-            "amplify:Get*",
-            "amplify:List*",
+            "cognito-identity:CreateIdentityPool",
+            "cognito-idp:ListIdentityProviders",
             "iam:CreateRole",
-            "iam:DeleteRole",
-            "iam:CreatePolicy",
-            "iam:DeletePolicy",
-            "iam:PutRolePolicy",
-            "iam:DeleteRolePolicy",
-            "iam:CreatePolicyVersion",
-            "iam:DeletePolicyVersion",
             "iam:AttachRolePolicy",
+            "iam:PutRolePolicy",
+            "cognito-idp:DeleteUserPoolDomain",
+            "cognito-idp:CreateIdentityProvider",
+            "amplify:Get*",
+            "cognito-idp:DescribeUserPool",
             "iam:DetachRolePolicy",
-            "cognito-identity:DescribeIdentityPool"
+            "cognito-idp:ListResourceServers",
+            "cognito-identity:UpdateIdentityPool",
+            "cognito-idp:DeleteGroup",
+            "cognito-idp:UpdateResourceServer",
+            "cognito-idp:DeleteResourceServer",
+            "cognito-idp:DeleteUserPoolClient",
+            "cognito-idp:UpdateUserPoolClient",
+            "iam:DeleteRole",
+            "cognito-idp:ListUserPools",
+            "cognito-idp:CreateResourceServer",
+            "cognito-idp:CreateUserPoolClient",
+            "cognito-identity:DeleteIdentityPool",
+            "amplify:List*",
+            "cognito-idp:UpdateUserPoolDomain",
+            "cognito-idp:DeleteUserPool",
+            "cognito-identity:ListIdentityPools",
+            "cognito-idp:CreateGroup",
+            "cognito-idp:CreateUserPool",
+            "iam:DeletePolicy",
+            "cognito-idp:UpdateIdentityProvider",
+            "cognito-idp:ListUserPoolClients",
+            "amplify:*",
+            "cognito-idp:CreateUserPoolDomain",
+            "cognito-idp:ListGroups",
+            "cognito-idp:DescribeIdentityProvider",
+            "iam:DeleteRolePolicy",
+            "cognito-idp:UpdateGroup",
+            "cognito-identity:SetIdentityPoolRoles",
+            "cognito-idp:DescribeResourceServer",
+            "iam:CreatePolicyVersion",
+            "cognito-idp:DescribeUserPoolClient",
+            "cognito-identity:DescribeIdentityPool",
+            "cognito-idp:DeleteIdentityProvider",
+            "cognito-idp:ListTagsForResource",
+            "iam:CreatePolicy",
+            "cognito-idp:DescribeUserPoolDomain",
+            "cognito-identity:ListTagsForResource",
+            "cognito-idp:UpdateUserPool",
+            "iam:DeletePolicyVersion",
+            "cognito-idp:ListUserImportJobs"
         ]
 
         policy = aws_iam.ManagedPolicy(self,
             "AmplifyCodeBuildScriptRunnerPolicy",
             managed_policy_name="AmplifyCodeBuildScriptRunnerPolicy",
             description="Policy used by the CodeBuild role that manages the creation of backend resources using the Amplify CLI",
-            # document=aws_iam.PolicyDocument(
             statements=[
                 aws_iam.PolicyStatement(actions=individual_actions, effect=aws_iam.Effect.ALLOW, resources=["*"]),
             ]
-            # )
-
         )
 
         policy.attach_to_role(project.role)
