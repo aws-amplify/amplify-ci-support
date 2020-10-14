b8a910b281dfe59c60bde08f095cc7ba368c340e
Rafael Juliano <rjjulian@amazon.com>
2020-10-14 15:26:22 -0400

    Created deployment scripts for different tests

--- src/integ_test_resources/android/amplify/integration/cdk/app.py
+++ src/integ_test_resources/android/amplify/integration/cdk/app.py
@@ -3,7 +3,25 @@
 from aws_cdk import core
 from stacks.amplify_deployer_stack import AmplifyDeployer
 
+def build_amplify_deployer_stack_props(cb_project_name: str, 
+                                        github_repo: str, 
+                                        github_owner: str, 
+                                        branch: str,
+                                        shell_script_name: str):
+    props = {}
+    props['cb_project_name'] = cb_project_name
+    props['shell_script_name'] = shell_script_name
+    props['github_repo'] = github_repo
+    if github_owner is not None:
+        props['github_owner'] = github_owner
+    if branch is not None:
+        props['branch'] = branch
+    return props
+
 app = core.App()
+github_owner=app.node.try_get_context("github_owner")
+branch=app.node.try_get_context("branch")
+
 TARGET_REGION = app.node.try_get_context("region")
 TARGET_ACCOUNT = app.node.try_get_context("account")
 if TARGET_ACCOUNT is None or TARGET_REGION is None:
@@ -11,26 +29,26 @@ if TARGET_ACCOUNT is None or TARGET_REGION is None:
 
 TARGET_ENV = core.Environment( account=TARGET_ACCOUNT, region=TARGET_REGION)
 GITHUB_REPO = 'amplify-ci-support'
-
 BANNER_TEXT = f"AWS Account={TARGET_ACCOUNT} Region={TARGET_REGION}"
 SEPARATOR = "-" * len(BANNER_TEXT)
-
-github_owner=app.node.try_get_context("github_owner")
-branch=app.node.try_get_context("branch")
 
 print(SEPARATOR)
 print(BANNER_TEXT)
 print(SEPARATOR)
 
-props = {}
-props['cb_project_name'] = "AmplifyAuthScenariosDeployer"
-props['buildspec_file_path'] = 'src/integ_test_resources/android/amplify/integration/cdk/scripts/buildspec.yml'
-props['github_repo'] = GITHUB_REPO
-if github_owner is not None:
-    props['github_owner'] = github_owner
-if branch is not None:
-    props['branch'] = branch
+at_auth_props = build_amplify_deployer_stack_props(cb_project_name="AmplifyAuthScenariosDeployer",
+                                                    github_repo=GITHUB_REPO,
+                                                    github_owner=github_owner,
+                                                    branch=branch,
+                                                    shell_script_name="deploy_at_auth_backend.sh")
 
-AmplifyDeployer(app, "AtAuthTestBackend", props, env=TARGET_ENV)
+instrumented_test_props = build_amplify_deployer_stack_props(cb_project_name="InstrumentedTestBackendDeployer",
+                                                    github_repo=GITHUB_REPO,
+                                                    github_owner=github_owner,
+                                                    branch=branch,
+                                                    shell_script_name="deploy_instrumented_tests_backend.sh")                                                  
+
+at_auth_backend_stack = AmplifyDeployer(app, "AtAuthScenariosBackend", at_auth_props, env=TARGET_ENV)
+instrumented_test_backend_stack = AmplifyDeployer(app, "InstrumentedTestsBackend", instrumented_test_props, env=TARGET_ENV)
 
 app.synth()
--- src/integ_test_resources/android/amplify/integration/cdk/stacks/amplify_deployer_stack.py
+++ src/integ_test_resources/android/amplify/integration/cdk/stacks/amplify_deployer_stack.py
@@ -9,10 +9,13 @@ from aws_cdk import (
 
 class AmplifyDeployer(core.Stack):
     DEFAULT_GITHUB_OWNER = "aws-amplify"
-    DEFAULT_BRANCH = "refs/heads/main"
+    DEFAULT_BRANCH = "main"
+    SCRIPTS_PATH = "src/integ_test_resources/android/amplify/integration/cdk/scripts"
+
     def __init__(self, scope: core.App, id: str, props, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        cb_buildspec_file_path = props['cb_buildspec_file_path']
+        shell_script_name = props['shell_script_name']
+        cb_project_name = props['cb_project_name']
         github_repo = props['github_repo']
         github_owner  = self.DEFAULT_GITHUB_OWNER if 'github_owner' not in props else props['github_owner']
         branch  = self.DEFAULT_BRANCH if 'branch' not in props else props['branch']
@@ -21,70 +24,68 @@ class AmplifyDeployer(core.Stack):
                                                             compute_type=aws_codebuild.ComputeType.SMALL)
         
         project = aws_codebuild.Project(self,
-                                        props['project_name'],
+                                        cb_project_name,
                                         source=aws_codebuild.Source.git_hub(owner=github_owner, 
                                                                             report_build_status=False,
                                                                             repo=github_repo, 
                                                                             branch_or_ref=branch,
                                                                             webhook=False), # Will need to setup creds to make this true
                                         environment=build_environment,
-                                        build_spec=aws_codebuild.BuildSpec.from_source_filename(filename=cb_buildspec_file_path))
+                                        build_spec=self._generate_buildspec(shell_script_name))
         individual_actions = [
-            "cognito-identity:CreateIdentityPool",
-            "cognito-idp:ListIdentityProviders",
-            "iam:CreateRole",
-            "iam:AttachRolePolicy",
-            "iam:PutRolePolicy",
-            "cognito-idp:DeleteUserPoolDomain",
-            "cognito-idp:CreateIdentityProvider",
-            "amplify:Get*",
-            "cognito-idp:DescribeUserPool",
-            "iam:DetachRolePolicy",
-            "cognito-idp:ListResourceServers",
-            "cognito-identity:UpdateIdentityPool",
-            "cognito-idp:DeleteGroup",
-            "cognito-idp:UpdateResourceServer",
-            "cognito-idp:DeleteResourceServer",
-            "cognito-idp:DeleteUserPoolClient",
-            "cognito-idp:UpdateUserPoolClient",
-            "iam:DeleteRole",
-            "cognito-idp:ListUserPools",
-            "cognito-idp:CreateResourceServer",
-            "cognito-idp:CreateUserPoolClient",
-            "cognito-identity:DeleteIdentityPool",
-            "amplify:List*",
-            "cognito-idp:UpdateUserPoolDomain",
-            "cognito-idp:DeleteUserPool",
-            "cognito-identity:ListIdentityPools",
-            "cognito-idp:CreateGroup",
-            "cognito-idp:CreateUserPool",
-            "iam:DeletePolicy",
-            "cognito-idp:UpdateIdentityProvider",
-            "cognito-idp:ListUserPoolClients",
             "amplify:*",
-            "cognito-idp:CreateUserPoolDomain",
-            "cognito-idp:ListGroups",
-            "cognito-idp:DescribeIdentityProvider",
-            "iam:DeleteRolePolicy",
-            "cognito-idp:UpdateGroup",
-            "cognito-identity:SetIdentityPoolRoles",
-            "cognito-idp:DescribeResourceServer",
-            "iam:CreatePolicyVersion",
-            "cognito-idp:DescribeUserPoolClient",
+            "cognito-identity:CreateIdentityPool",
+            "cognito-identity:DeleteIdentityPool",
             "cognito-identity:DescribeIdentityPool",
-            "cognito-idp:DeleteIdentityProvider",
-            "cognito-idp:ListTagsForResource",
-            "iam:CreatePolicy",
-            "cognito-idp:DescribeUserPoolDomain",
+            "cognito-identity:ListIdentityPools",
             "cognito-identity:ListTagsForResource",
+            "cognito-identity:SetIdentityPoolRoles",
+            "cognito-identity:UpdateIdentityPool",
+            "cognito-idp:CreateGroup",
+            "cognito-idp:CreateIdentityProvider",
+            "cognito-idp:CreateResourceServer",
+            "cognito-idp:CreateUserPool",
+            "cognito-idp:CreateUserPoolClient",
+            "cognito-idp:CreateUserPoolDomain",
+            "cognito-idp:DeleteGroup",
+            "cognito-idp:DeleteIdentityProvider",
+            "cognito-idp:DeleteResourceServer",
+            "cognito-idp:DeleteUserPool",
+            "cognito-idp:DeleteUserPoolClient",
+            "cognito-idp:DeleteUserPoolDomain",
+            "cognito-idp:DescribeIdentityProvider",
+            "cognito-idp:DescribeResourceServer",
+            "cognito-idp:DescribeUserPool",
+            "cognito-idp:DescribeUserPoolClient",
+            "cognito-idp:DescribeUserPoolDomain",
+            "cognito-idp:ListGroups",
+            "cognito-idp:ListIdentityProviders",
+            "cognito-idp:ListResourceServers",
+            "cognito-idp:ListTagsForResource",
+            "cognito-idp:ListUserImportJobs",
+            "cognito-idp:ListUserPoolClients",
+            "cognito-idp:ListUserPools",
+            "cognito-idp:UpdateGroup",
+            "cognito-idp:UpdateIdentityProvider",
+            "cognito-idp:UpdateResourceServer",
             "cognito-idp:UpdateUserPool",
+            "cognito-idp:UpdateUserPoolClient",
+            "cognito-idp:UpdateUserPoolDomain",
+            "iam:AttachRolePolicy",
+            "iam:CreatePolicy",
+            "iam:CreatePolicyVersion",
+            "iam:CreateRole",
+            "iam:DeletePolicy",
             "iam:DeletePolicyVersion",
-            "cognito-idp:ListUserImportJobs"
+            "iam:DeleteRole",
+            "iam:DeleteRolePolicy",
+            "iam:DetachRolePolicy",
+            "iam:PutRolePolicy",
         ]
 
         policy = aws_iam.ManagedPolicy(self,
             "AmplifyCodeBuildScriptRunnerPolicy",
-            managed_policy_name="AmplifyCodeBuildScriptRunnerPolicy",
+            managed_policy_name=f"AmplifyCodeBuildScriptRunnerPolicy-{cb_project_name}",
             description="Policy used by the CodeBuild role that manages the creation of backend resources using the Amplify CLI",
             statements=[
                 aws_iam.PolicyStatement(actions=individual_actions, effect=aws_iam.Effect.ALLOW, resources=["*"]),
@@ -98,3 +99,26 @@ class AmplifyDeployer(core.Stack):
         project.role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name('IAMReadOnlyAccess'))
         project.role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name('AWSLambdaFullAccess'))
         project.role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name('AWSAppSyncAdministrator'))
+    
+    def _generate_buildspec(self, shell_script_name: str):
+        deployer_command = f"{self.SCRIPTS_PATH}/{shell_script_name}"
+        return aws_codebuild.BuildSpec.from_object({
+            "version": "0.2",
+            "phases": {
+                "install": {
+                    "commands":[
+                        "echo 'Install phase starting'",
+                        "npm install -g @aws-amplify/cli"
+                    ]
+                },
+                "build": {
+                    "commands": [
+                        "echo 'Build phase starting'",
+                        "pwd",
+                        "ls -al",
+                        deployer_command
+                    ]
+                }
+            }
+        })
+        
