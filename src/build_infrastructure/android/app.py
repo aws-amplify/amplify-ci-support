3bcce6bb1639e88dd7eb9ba1c525f444f163429e
Rafael Juliano <rjjulian@amazon.com>
2021-01-05 11:21:59 -0500

    Adding build infrastructure for amplify-android (#69)

--- src/build_infrastructure/android/app.py
+++ src/build_infrastructure/android/app.py
@@ -1,28 +1,45 @@
 #!/usr/bin/env python3
 
 import os
-from aws_cdk import core
-from cdk_build_pipeline.build_pipeline_stack import BuildPipelineStack
+from aws_cdk import (
+    core,
+    aws_codepipeline_actions
+)
+
+from stacks.build_pipeline_stack import AmplifyAndroidCodePipeline
+from stacks.account_bootstrap_stack import AccountBootstrap
 
 app = core.App()
 TARGET_REGION = app.node.try_get_context("region")
 TARGET_ACCOUNT = app.node.try_get_context("account")
 TARGET_ENV=core.Environment( account=TARGET_ACCOUNT, region=TARGET_REGION)
+REPO='amplify-android'
 
 github_owner=app.node.try_get_context("github_owner")
 branch=app.node.try_get_context("branch")
-
+config_source_bucket = app.node.try_get_context("config_source_bucket")
 print(f"AWS Account={TARGET_ACCOUNT} Region={TARGET_REGION}")
+log_level=app.node.try_get_context("log_level")
 
-props = {}
-if github_owner is not None:
-    props['github_owner'] = github_owner
-if branch is not None:
-    props['branch'] = branch
+account_bootstrap = AccountBootstrap(app, "AccountBootstrap", {}, env=TARGET_ENV)
 
-pipeline_stack = BuildPipelineStack(app, 
+code_pipeline_stack_props = {
+    # If set, config files for tests will be copied from S3. Otherwise, it will attempt to retrieve using the Amplify CLI
+    'config_source_bucket': account_bootstrap.config_source_bucket.bucket_name, 
+    'github_source': { 
+        'owner': github_owner, 
+        'repo': REPO , 
+        'base_branch': branch 
+    },
+    'device_farm_project_name': 'AmplifyAndroidDeviceFarmTests',
+    'codebuild_project_name_prefix': 'AmplifyAndroid'
+}
+
+pipeline_stack = AmplifyAndroidCodePipeline(app,
                                     "AndroidBuildPipeline",
-                                    props,
+                                    code_pipeline_stack_props,
+                                    description="CI Pipeline assets for amplify-android",
                                     env=TARGET_ENV)
+
 
 app.synth()
