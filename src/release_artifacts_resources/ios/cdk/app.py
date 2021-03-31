28c0500215f2338bc789eb6eb73b22ad8bab00af
Jithin Roy <royji@amazon.com>
2021-03-31 10:15:31 -0700

    Moved stack to construct

--- src/release_artifacts_resources/ios/cdk/app.py
+++ src/release_artifacts_resources/ios/cdk/app.py
@@ -1,38 +1,11 @@
 #!/usr/bin/env python3
 
-from aws_cdk import core as cdk
-from cdk.s3_stack import S3Stack
-from cdk.cloudfront_stack import CloudFrontStack
-
 from aws_cdk import core
-
-from cdk.cdk_stack import CdkStack
-
-def raiseOperationValueError():
-    raise ValueError(
-        "Provide operation with value either 'CREDENTIAL_ROTATION' or 'RELEASE_HOST' in 'context' parameter, as in: cdk deploy app -c operation=CREDENTIAL_ROTATION"  # noqa: E501
-    )
+from cdk.credential_rotation_stack import CredentialRotationStack
+from cdk.distribution_stack import DistributionStack
 
 app = core.App()
-
-operation = app.node.try_get_context("operation")
-if operation is None:
-    raiseOperationValueError()
-
-main = CdkStack(app, "MainStack")
-
-if operation == 'RELEASE_HOST':
-    
-    s3_stack = S3Stack(app, "S3Stack")
-    cloudfront_stack = CloudFrontStack(app, "CloudfrontStack", s3_stack)
-    cloudfront_stack.add_dependency(s3_stack)
-
-    main.add_dependency(s3_stack)
-    main.add_dependency(cloudfront_stack)
-# elif operation == 'CREDENTIAL_ROTATION':
-#     # iam_stack = IAMStack(app, "IAMStack", bucket_arn="sd", cloudfront_arn="sd")
-#     # lambda_stack = LambdaStack(app, "LambdaStack", iam_stack=iam_stack)
-else:
-    raiseOperationValueError()
+distribution_stack = DistributionStack(app, "DistributionStack")
+credential_rotation_stack = CredentialRotationStack(app, "CredentialRotationStack")
 
 app.synth()
---
+++ src/release_artifacts_resources/ios/cdk/cdk/distribution_stack.py
@@ -0,0 +1,11 @@
+from aws_cdk import core as cdk
+from cdk.distribution.cloudfront_construct import CloudFrontConstruct
+from cdk.distribution.s3_construct import S3Construct
+
+
+class DistributionStack(cdk.Stack):
+    def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        s3 = S3Construct(self, "distribution_s3_construct")
+        CloudFrontConstruct(self, "distribution_cloudfront_construct", s3)
