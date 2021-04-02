02b615e8bc7f11c006e06c2efb8430ab83b5df09
Jithin Roy <royji@amazon.com>
2021-04-02 16:26:19 -0700

    chore: Added versioning, logging and encryption

--- src/release_artifacts_resources/ios/cdk/cdk/distribution/s3_construct.py
+++ src/release_artifacts_resources/ios/cdk/cdk/distribution/s3_construct.py
@@ -5,6 +5,15 @@ class S3Construct(core.Construct):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        logging_bucket_name = "aws-sdk-ios-release-access-logging"
+        access_log_bucket = aws_s3.Bucket(
+            self,
+            "release_artifacts_ios_v2_sdk_release_bucket_access_logging",
+            bucket_name=logging_bucket_name,
+            block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=core.RemovalPolicy.RETAIN,
+        )
+
         bucket_name = "aws-sdk-ios-release"
 
         self.bucket = aws_s3.Bucket(
@@ -13,4 +22,7 @@ class S3Construct(core.Construct):
             bucket_name=bucket_name,
             block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
             removal_policy=core.RemovalPolicy.RETAIN,
+            server_access_logs_bucket=access_log_bucket,
+            versioned=True,
+            encryption=aws_s3.BucketEncryption.S3_MANAGED
         )
