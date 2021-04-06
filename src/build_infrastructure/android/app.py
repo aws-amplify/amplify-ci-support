412b81f5eff5106c5c5a2e4e81b232a5cac6c350
Rafael Juliano <rjjulian@amazon.com>
2021-04-06 10:35:17 -0400

    chore(android): add CodeBuild project for release automation

--- src/build_infrastructure/android/app.py
+++ src/build_infrastructure/android/app.py
@@ -8,6 +8,7 @@ from aws_cdk import (
 
 from stacks.build_pipeline_stack import AmplifyAndroidCodePipeline
 from stacks.account_bootstrap_stack import AccountBootstrap
+from stacks.maven_release_stack import MavenReleaseStack
 
 app = core.App()
 TARGET_REGION = app.node.try_get_context("region")
@@ -18,11 +19,15 @@ REPO='amplify-android'
 github_owner=app.node.try_get_context("github_owner")
 branch=app.node.try_get_context("branch")
 config_source_bucket = app.node.try_get_context("config_source_bucket")
-print(f"AWS Account={TARGET_ACCOUNT} Region={TARGET_REGION}")
+release_pr_branch = app.node.try_get_context("release_pr_branch")
 log_level=app.node.try_get_context("log_level")
 
+print(f"AWS Account={TARGET_ACCOUNT} Region={TARGET_REGION}")
+
+# Account bootstrap stack
 account_bootstrap = AccountBootstrap(app, "AccountBootstrap", {}, env=TARGET_ENV)
 
+# Unit and integration test stack
 code_pipeline_stack_props = {
     # If set, config files for tests will be copied from S3. Otherwise, it will attempt to retrieve using the Amplify CLI
     'config_source_bucket': account_bootstrap.config_source_bucket.bucket_name, 
@@ -40,6 +45,19 @@ pipeline_stack = AmplifyAndroidCodePipeline(app,
                                     code_pipeline_stack_props,
                                     description="CI Pipeline assets for amplify-android",
                                     env=TARGET_ENV)
+
+# Maven publisher stack
+maven_publisher_stack_props = {
+    'github_source': { 
+        'owner': github_owner,
+        'repo': REPO, 
+        'base_branch': branch,
+        'release_pr_branch': release_pr_branch
+    },
+    'codebuild_project_name_prefix': 'AmplifyAndroid'
+}
+
+MavenReleaseStack(app, "MavenPublisher", maven_publisher_stack_props, description="Assets used for publishing amplify-android to maven.", env=TARGET_ENV)
 
 
 app.synth()
