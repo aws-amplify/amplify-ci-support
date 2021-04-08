246e3b23d97f5083cece6c314e92e50aa5c348c4
Rafael Juliano <rjjulian@amazon.com>
2021-04-08 09:37:34 -0400

    chore(android): add CodeBuild project for release automation (#81)

--- src/build_infrastructure/android/amplify_custom_resources/__init__.py
+++ src/build_infrastructure/android/amplify_custom_resources/__init__.py
@@ -3,4 +3,5 @@ import sys
 from .device_farm_project import DeviceFarmProject
 from .device_farm_device_pool import DeviceFarmDevicePool
 from .pull_request_builder import PullRequestBuilder
+from .maven_release_publisher import MavenPublisher
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "."))
