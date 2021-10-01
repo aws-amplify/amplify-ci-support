16211227e30e7836adbe72c522bc63810798aaee
Chris Ameter <29186822+ameter@users.noreply.github.com>
2021-10-01 16:43:58 -0600

    applied PR comments

--- src/integ_test_resources/ios/sdk/integration/cdk/cdk_integration_tests_ios/location_stack.py
+++ src/integ_test_resources/ios/sdk/integration/cdk/cdk_integration_tests_ios/location_stack.py
@@ -3,13 +3,16 @@ from common.common_stack import CommonStack
 from common.platforms import Platform
 from common.region_aware_stack import RegionAwareStack
 
+
 class LocationStack(RegionAwareStack):
-    def __init__(self, scope: core.Construct, id: str, common_stack: CommonStack, **kwargs) -> None:
+    def __init__(
+        self, scope: core.Construct, id: str, common_stack: CommonStack, **kwargs
+    ) -> None:
         super().__init__(scope, id, **kwargs)
 
         self._supported_in_region = self.is_service_supported_in_region()
 
-        ##### Map #####
+        # Map #
 
         # Reusing the get_bucket_name function to get a unique name for the map
         mapName = self.get_bucket_name("map")
@@ -18,9 +21,11 @@ class LocationStack(RegionAwareStack):
         aws_location.CfnMap(
             self,
             "integ_test_location_map",
-            configuration=aws_location.CfnMap.MapConfigurationProperty(style="VectorEsriStreets"),
+            configuration=aws_location.CfnMap.MapConfigurationProperty(
+                style="VectorEsriStreets"
+            ),
             map_name=mapName,
-            pricing_plan="RequestBasedUsage"
+            pricing_plan="RequestBasedUsage",
         )
 
         location_map_policy = aws_iam.PolicyStatement(
@@ -29,13 +34,15 @@ class LocationStack(RegionAwareStack):
                 "geo:GetMapStyleDescriptor",
                 "geo:GetMapGlyphs",
                 "geo:GetMapSprites",
-                "geo:GetMapTile"
+                "geo:GetMapTile",
             ],
-            resources=[f"arn:aws:geo:{self.region}:{self.account}:map/*"]
+            resources=[f"arn:aws:geo:{self.region}:{self.account}:map/*"],
         )
-        common_stack.add_to_common_role_policies(self, policy_to_add=location_map_policy)
+        common_stack.add_to_common_role_policies(
+            self, policy_to_add=location_map_policy
+        )
 
-        ##### Search #####
+        # Search #
 
         # Reusing the get_bucket_name function to get a unique name for the place index.
         placeIndexName = self.get_bucket_name("placeIndex")
@@ -46,17 +53,16 @@ class LocationStack(RegionAwareStack):
             "integ_test_location_placeIndex",
             data_source="Esri",
             index_name=placeIndexName,
-            pricing_plan="RequestBasedUsage"
+            pricing_plan="RequestBasedUsage",
         )
 
         location_placeIndex_policy = aws_iam.PolicyStatement(
             effect=aws_iam.Effect.ALLOW,
-            actions=[
-                "geo:SearchPlaceIndexForPosition",
-                "geo:SearchPlaceIndexForText"
-            ],
-            resources=[f"arn:aws:geo:{self.region}:{self.account}:place-index/*"]
+            actions=["geo:SearchPlaceIndexForPosition", "geo:SearchPlaceIndexForText"],
+            resources=[f"arn:aws:geo:{self.region}:{self.account}:place-index/*"],
         )
-        common_stack.add_to_common_role_policies(self, policy_to_add=location_placeIndex_policy)
+        common_stack.add_to_common_role_policies(
+            self, policy_to_add=location_placeIndex_policy
+        )
 
         self.save_parameters_in_parameter_store(platform=Platform.IOS)
