4e5cf859b045a263a7e92011dbb414bbe6fd3dfc
wlee221 <willeea@amazon.com>
2022-09-01 03:28:53 -0700

    Revert folder name change

--- src/credentials_rotators/npm-token-rotation/test/token-rotation.test.ts
+++ src/credentials_rotators/npm-token-rotation/test/token-rotation.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as TokenRotation from '../src/token-rotation-stack';
+import * as TokenRotation from '../src/stacks/npm-token-rotation';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new TokenRotation.TokenRotationStack(app, 'MyTestStack');
+    const stack = new TokenRotation.NpmTokenRotationStack(app, 'MyTestStack', {config: {} as any});
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
