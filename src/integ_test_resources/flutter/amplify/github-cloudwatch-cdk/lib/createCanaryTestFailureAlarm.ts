dc3139c7f594392c65d92c5709e05ab1781d9f61
Kyle <kylcheng@gmail.com>
2023-06-26 05:54:49 -0700

    PR Comments

--- src/integ_test_resources/flutter/amplify/github-cloudwatch-cdk/lib/createCanaryTestFailureAlarm.ts
+++ src/integ_test_resources/flutter/amplify/github-cloudwatch-cdk/lib/createCanaryTestFailureAlarm.ts
@@ -1,93 +1,72 @@
 import { Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Alarm, ComparisonOperator, GraphWidget, Metric, TextWidget, TreatMissingData } from 'aws-cdk-lib/aws-cloudwatch';
+import { Alarm, ComparisonOperator, Metric, TreatMissingData, DimensionsMap, Stats } from 'aws-cdk-lib/aws-cloudwatch';
+
+const metricNamespace = "GithubCanaryApps";
 
 // Create the metric and alarms for tracking Github Canary failures.
 // These metrics are sent in Github action: amplify-flutter/amplify_canaries
 export function createCanaryTestFailureAlarm(scope: Construct) {
 
-  const buildFailureMetric = new Metric({
-    metricName: "BuildCanaryTestFailure",
-    namespace: "GithubCanaryApps",
-    statistic: "Max",
-    period: Duration.hours(24),
-    dimensionsMap: {
-        "channel" : "stable"
+  const buildFailureMetric = createMetric(
+    "BuildCanaryTestFailure",
+    {
+      "channel": "stable",
+      "label": "Canary Build Failures",
     }
-  });
+  );
 
-  const e2eAndroidMetric = new Metric({
-    metricName: "e2eCanaryTestFailure",
-    namespace: "GithubCanaryApps",
-    statistic: "Max",
-    period: Duration.hours(24),
-    dimensionsMap: {
-        "channel" : "stable",
-        "platform" : "android"
+  const E2EAndroidMetric = createMetric(
+    "E2ECanaryTestFailure",
+    {
+      "channel": "stable",
+      "platform": "android",
+      "label": "E2E Canary Failures",
     }
-  });
+  );
 
-  const e2eIosMetric = new Metric({
-    metricName: "e2eCanaryTestFailure",
-    namespace: "GithubCanaryApps",
-    statistic: "Max",
-    period: Duration.hours(24),
-    dimensionsMap: {
-        "channel" : "stable",
-        "platform" : "ios"
+  const E2EIosMetric = createMetric(
+    "E2ECanaryTestFailure",
+    {
+      "channel": "stable",
+      "platform": "ios",
+      "label": "E2E Canary Failures",
     }
-  });
+  );
 
-  // TODO(kylechen): Create metrics for channel: beta
-  // TODO(kylehcen): Create Github issues as alarm actions
+  createAlarm(scope, `build-canary-test-failure-alarm`, "Build Canary Test Failure Alarm", "Alarm triggered when the Github Action Canaries build step fails", buildFailureMetric);
 
-  const buildFailureAlarm = new Alarm(scope, `build-canary-test-failure-alarm`, {
-    alarmName: "Build Canary Test Failure Alarm",
-    alarmDescription: `Alarm triggered when the Github Action Canaries build step fails`,
-    metric: buildFailureMetric,
-    threshold: 0,
-    evaluationPeriods: 1,
-    datapointsToAlarm: 1,
-    comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
-    actionsEnabled: true,
-    treatMissingData: TreatMissingData.MISSING,
-  });
+  createAlarm(scope, `e2e-android-canary-test-failure-alarm`, "E2E Android Canary Test Failure Alarm", "Alarm triggered when the Github Action Canaries E2E android step fails", E2EAndroidMetric);
 
-  const e2eAndroidAlarm = new Alarm(scope, `e2e-android-canary-test-failure-alarm`, {
-    alarmName: "e2e Android Canary Test Failure Alarm",
-    alarmDescription: `Alarm triggered when the Github Action Canaries e2e android step fails`,
-    metric: e2eAndroidMetric,
-    threshold: 0,
-    evaluationPeriods: 1,
-    datapointsToAlarm: 1,
-    comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
-    actionsEnabled: true,
-    treatMissingData: TreatMissingData.MISSING,
-  });
+  createAlarm(scope, `e2e-ios-canary-test-failure-alarm`, "E2E iOS Canary Test Failure Alarm", "Alarm triggered when the Github Action Canaries E2E ios step fails", E2EIosMetric);
 
-  const e2eIosAlarm = new Alarm(scope, `e2e-ios-canary-test-failure-alarm`, {
-    alarmName: "e2e iOS Canary Test Failure Alarm",
-    alarmDescription: `Alarm triggered when the Github Action Canaries e2e ios step fails`,
-    metric: e2eIosMetric,
-    threshold: 0,
-    evaluationPeriods: 1,
-    datapointsToAlarm: 1,
-    comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
-    actionsEnabled: true,
-    treatMissingData: TreatMissingData.MISSING,
-  });
+  // TODO: Create Github issues as alarm actions
 
-  // TODO(kylechen): Consider moving to brazil to allow SIM Ticket creation.
+  // TODO: Consider moving to brazil to allow SIM Ticket creation.
   // reason: package 'SIMTicketAlarmAction' is an AWS internal package.
-  /*
-  import { SIMTicketAlarmAction, Severity } from "@amzn/sim-ticket-cdk-constructs";
-  const severity = Severity.SEV2_5;
-  createCanaryTestFailureAlarm.addAlarmAction(new SIMTicketAlarmAction({
-    severity,
-    dedupe: `github-build-and-runtime-test-failure-alarm-${stage}`,
-    summary: "There's a failure on Github Actions when running 'Run and Test Builds' workflow.",
-    details: "Check what failed -> https://github.com/aws-amplify/amplify-ui/actions/workflows/build-and-runtime-test.yml",
-    ...CTI_GROUP
-  }));
-  */
+
+}
+
+function createMetric(metricName: string, dimensions: DimensionsMap): Metric {
+  return new Metric({
+    metricName,
+    namespace: metricNamespace,
+    statistic: Stats.MAXIMUM,
+    period: Duration.hours(24),
+    dimensionsMap: dimensions,
+  });
+}
+
+function createAlarm(scope: Construct, id: string, alarmName: string, alarmDescription: string, metric: Metric) {
+  return new Alarm(scope, id, {
+    alarmName: alarmName,
+    alarmDescription: alarmDescription,
+    metric: metric,
+    threshold: 0,
+    evaluationPeriods: 1,
+    datapointsToAlarm: 1,
+    comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
+    actionsEnabled: true,
+    treatMissingData: TreatMissingData.IGNORE,
+  });
 }
