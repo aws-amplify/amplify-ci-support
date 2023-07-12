43f34dcb909d770019353a3a2624e8461e47dc2c
Di Wu <diwud@amazon.com>
2023-07-12 15:19:31 -0700

    test: add github action assumed role for aws-sdk-ios integration test

--- src/integ_test_resources/ios/sdk/integration/cdk/cdk_integration_tests_ios/sts_stack.py
+++ src/integ_test_resources/ios/sdk/integration/cdk/cdk_integration_tests_ios/sts_stack.py
@@ -10,9 +10,17 @@ class StsStack(RegionAwareStack):
 
         self._supported_in_region = self.is_service_supported_in_region()
 
+        assumed_by_principal = common_stack.circleci_execution_role
+        if common_stack.github_action_oidc is not None:
+            assumed_by_principal = aws_iam.CompositePrincipal(
+                common_stack.circleci_execution_role,
+                common_stack.github_action_oidc.aws_sdk_ios_integration_test_role
+            )
+
         # Create a role with no permissions to test the SDKs assumeRole API
         test_role = aws_iam.Role(
-            self, "sts_test_role", assumed_by=common_stack.circleci_execution_role
+            self, "sts_test_role",
+            assumed_by=assumed_by_principal
         )
         self._parameters_to_save = {"testRoleArn": test_role.role_arn}
         self.save_parameters_in_parameter_store(platform=Platform.IOS)
