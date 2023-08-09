efa8c5ac8de674aa385adfea69082d9368bd3005
Di Wu <im@wudi.me>
2023-08-09 10:16:00 -0700

    ci: remove redundant logs in integration test lambda functions (#158)

--- src/integ_test_resources/ios/sdk/integration/cdk/custom_resources/iot_custom_authorizer_provider/iot_custom_authorizer_provider.py
+++ src/integ_test_resources/ios/sdk/integration/cdk/custom_resources/iot_custom_authorizer_provider/iot_custom_authorizer_provider.py
@@ -5,7 +5,6 @@ physical_id = "iot_custom_authorizer_provider_20200508074521"
 
 
 def on_event(event, __):
-    print(f"### on_event({event})")
     request_type = event["RequestType"]
     if request_type == "Create":
         return on_create_or_update(event)
@@ -17,7 +16,6 @@ def on_event(event, __):
 
 
 def on_create_or_update(event):
-    print(f"### on_create_or_update({event})")
     custom_authorizer_function_arn = event["ResourceProperties"]["authorizer_function_arn"]
     custom_authorizer_name = event["ResourceProperties"]["authorizer_name"]
     public_key = event["ResourceProperties"]["public_key"]
@@ -26,21 +24,17 @@ def on_create_or_update(event):
         custom_authorizer_name, custom_authorizer_function_arn, public_key, token_key_name
     )
     response = {"PhysicalResourceId": physical_id}
-    print(f"### on_create_or_update response: {response}")
     return response
 
 
 def on_delete(event):
-    print(f"### on_delete({event})")
     custom_authorizer_name = event["ResourceProperties"]["authorizer_name"]
     delete_authorizer(custom_authorizer_name)
     response = {"PhysicalResourceId": physical_id}
-    print(f"### on_delete response: {response}")
     return response
 
 
 def create_authorizer(name, function_arn, public_key, token_key_name):
-    print(f"### create_authorizer({name, function_arn, public_key, token_key_name})")
     client = boto3.client("iot")
     formatted_key = format_public_key(public_key)
     create_authorizer_response = client.create_authorizer(
@@ -51,7 +45,6 @@ def create_authorizer(name, function_arn, public_key, token_key_name):
         signingDisabled=False,
         tokenSigningPublicKeys={"public_key": formatted_key},
     )
-    print(f"### create_authorizer_response: {create_authorizer_response}")
 
 
 def format_public_key(public_key):
@@ -59,11 +52,8 @@ def format_public_key(public_key):
 
 
 def delete_authorizer(name):
-    print(f"### delete_authorizer({name})")
     client = boto3.client("iot")
 
     update_authorizer_response = client.update_authorizer(authorizerName=name, status="INACTIVE")
-    print(f"### update_authorizer_response: {update_authorizer_response}")
 
     delete_authorizer_response = client.delete_authorizer(authorizerName=name)
-    print(f"### delete_authorizer_response: {delete_authorizer_response}")
--- src/integ_test_resources/ios/sdk/integration/cdk/custom_resources/iot_custom_authorizer_user_pass_provider/iot_custom_authorizer_user_pass_provider.py
+++ src/integ_test_resources/ios/sdk/integration/cdk/custom_resources/iot_custom_authorizer_user_pass_provider/iot_custom_authorizer_user_pass_provider.py
@@ -6,7 +6,6 @@ import boto3
 
 
 def on_event(event, __):
-    print(f"### on_event({event})")
     request_type = event["RequestType"]
     if request_type == "Create":
         return on_create_or_update(event)
@@ -18,7 +17,6 @@ def on_event(event, __):
 
 
 def on_create_or_update(event):
-    print(f"### on_create_or_update({event})")
     physical_id = os.environ["custom_auth_user_pass_uuid"]
     custom_authorizer_function_arn = event["ResourceProperties"]["authorizer_function_arn"]
     custom_authorizer_name = event["ResourceProperties"]["authorizer_name"]
@@ -34,7 +32,6 @@ def on_create_or_update(event):
         "PhysicalResourceId": physical_id,
         "Data": {"BetaEndpointAddress": beta_endpoint_addr},
     }
-    print(f"### on_create_or_update response: {response}")
     return response
 
 
@@ -47,7 +44,7 @@ def create_or_update_domain_config():
         create_domain_configuration_response = client.create_domain_configuration(
             domainConfigurationName=domain_configuration_name
         )
-        print(f"### create_domain_configuration response: {create_domain_configuration_response}")
+
     except client.exceptions.ResourceAlreadyExistsException:
         print("Already defined, continuing")
 
@@ -56,31 +53,25 @@ def create_or_update_domain_config():
         domainConfigurationStatus="ENABLED",
         authorizerConfig={"defaultAuthorizerName": default_authorizer_name},
     )
-    print(f"### update_domain_configuration response: {update_domain_configuration_response}")
 
     describe_domain_configuration_response = client.describe_domain_configuration(
         domainConfigurationName=domain_configuration_name
     )
-    print(f"### describe_domain_configuration response: {describe_domain_configuration_response}")
 
     beta_endpoint_addr = describe_domain_configuration_response["domainName"]
-    print(f"### beta_endpoint_addr: {beta_endpoint_addr}")
 
     return beta_endpoint_addr
 
 
 def on_delete(event):
-    print(f"### on_delete({event})")
     physical_id = os.environ["custom_auth_user_pass_uuid"]
     custom_authorizer_name = event["ResourceProperties"]["authorizer_name"]
     delete_authorizer(custom_authorizer_name)
     response = {"PhysicalResourceId": physical_id}
-    print(f"### on_delete response: {response}")
     return response
 
 
 def create_authorizer(name, function_arn, public_key, token_key_name):
-    print(f"### create_authorizer({name, function_arn, public_key, token_key_name})")
     client = boto3.client("iot")
     formatted_key = format_public_key(public_key)
     create_authorizer_response = client.create_authorizer(
@@ -91,7 +82,6 @@ def create_authorizer(name, function_arn, public_key, token_key_name):
         signingDisabled=False,
         tokenSigningPublicKeys={"public_key": formatted_key},
     )
-    print(f"### create_authorizer_response: {create_authorizer_response}")
 
 
 def format_public_key(public_key):
@@ -99,7 +89,6 @@ def format_public_key(public_key):
 
 
 def delete_authorizer(name):
-    print(f"### delete_authorizer({name})")
     client = boto3.client("iot")
     domain_configuration_name = os.environ["custom_auth_user_pass_domain_configuration_name"]
 
@@ -117,7 +106,5 @@ def delete_authorizer(name):
     # )
 
     update_authorizer_response = client.update_authorizer(authorizerName=name, status="INACTIVE")
-    print(f"### update_authorizer_response: {update_authorizer_response}")
 
     delete_authorizer_response = client.delete_authorizer(authorizerName=name)
-    print(f"### delete_authorizer_response: {delete_authorizer_response}")
