0483c76409f313343f7d58ba126841d8dba62dfc
Di Wu <diwud@amazon.com>
2024-04-18 13:26:09 -0700

    ci: remove credential rotation stack for release artifact CDK app (#162)

--- src/release_artifacts_resources/ios/cdk/app.py
+++ src/release_artifacts_resources/ios/cdk/app.py
@@ -1,28 +1,10 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-from cdk.credential_rotation_stack import CredentialRotationStack
 from cdk.distribution_stack import DistributionStack
 
 app = core.App()
 
-distribution_stack = DistributionStack(app, "DistributionStack")
-
-bucket_arn = distribution_stack.s3.bucket.bucket_arn
-bucket_name = distribution_stack.s3.bucket.bucket_name
-cloudfront_distribution_id = distribution_stack.cloudfront.distribution.distribution_id
-arn_components = core.ArnComponents(
-    resource="distribution/" + cloudfront_distribution_id, service="cloudfront", region=""
-)
-cloudfront_arn = core.Arn.format(components=arn_components, stack=distribution_stack)
-
-credential_rotation_stack = CredentialRotationStack(
-    app,
-    "CredentialRotationStack",
-    bucket_name=bucket_name,
-    bucket_arn=bucket_arn,
-    cloudfront_distribution_id=cloudfront_distribution_id,
-    cloudfront_arn=cloudfront_arn,
-)
+DistributionStack(app, "DistributionStack")
 
 app.synth()
--- src/release_artifacts_resources/ios/cdk/cdk/credential_rotation/lambda_functions/src/models/destination_type.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class DestinationType(str, Enum):
-    """A destination that rotated values are pushed to"""
-
-    CIRCLECI_ENVIRONMENT_VARIABLE = "cci_env_variable"
